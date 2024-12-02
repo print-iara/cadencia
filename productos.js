86d6cb44ba7a2ce4df3e64777b29f4f2d59854bb
iaraolivera <dev.iaraolivera@gmail.com>
2024-12-01 21:27:03 -0300

    implementando carrito

--- productos.js
+++ productos.js
@@ -1,13 +1,25 @@
-
 const containerCards = document.getElementById("container-cards");
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+const abrirCarrito = document.getElementById("abrir-carrito");
+const cerrarCarritoBtn = document.getElementById("cerrar-carrito");
+const carritoModal = document.getElementById("carrito");
+const vaciarCarritoBtn = document.getElementById("vaciar-carrito");
+const totalCompra = document.getElementById("total-compra");
+let carrito = [];
 
+// Abrir y cerrar carrito
+abrirCarrito.addEventListener("click", () => {
+  carritoModal.classList.add("abierto");
+});
 
+cerrarCarritoBtn.addEventListener("click", () => {
+  carritoModal.classList.remove("abierto");
+});
 
 
 async function obtenerProductos() {
   const respuesta = await fetch("https://fakestoreapi.com/products");
   const productos = await respuesta.json();
-
 
   productos.forEach((producto) => {
     const item = document.createElement("div");
@@ -22,9 +34,46 @@ async function obtenerProductos() {
       </button>
       </div>
     `;
-    console.log(` Producto=>\n ID: ${producto.id} \n Nombre: ${producto.title}\n Precio:$ ${producto.price}`)
+
+    
+    // console.log(` Producto=>\n ID: ${producto.id} \n Nombre: ${producto.title}\n Precio:$ ${producto.price}`)
     containerCards.appendChild(item);
   });
-
+  document.querySelectorAll(".btn_comprar").forEach((boton)=>{
+    boton.addEventListener("click", agregarProductoCarrito)
+  })
 }
 obtenerProductos();
+
+
+const agregarProductoCarrito=(e)=>{
+  carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+  e.preventDefault();
+  const id = e.target.dataset.id;
+  const nombre = e.target.dataset.nombre;
+  const precio = parseFloat(e.target.dataset.precio);
+  const imagen = e.target.dataset.imagen;
+
+  const productoExistente = carrito.find((item) => item.id === id);
+  if (productoExistente) {
+    productoExistente.cantidad++;
+  } else {
+    carrito.push({
+      id,
+      nombre,
+      precio,
+      imagen,
+      cantidad: 1,
+    });
+  }
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+  mostrar();
+
+}
+const mostrar=()=>{
+  console.log("lista completa:")
+  carrito.forEach((item)=>{
+    console.log(`Producto: ${item.nombre} - Precio: $${item.precio}- cantidad: ${item.cantidad}`)
+  })
+}
+
